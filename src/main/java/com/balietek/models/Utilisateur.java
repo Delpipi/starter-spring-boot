215a4602d25e16187d74a09ae953c889e9d2a1bc
Delpipi <alexandrepaulkouame@gmail.com>
2023-01-03 18:14:29 +0000

    setup security

--- src/main/java/com/balietek/models/Utilisateur.java
+++ src/main/java/com/balietek/models/Utilisateur.java
@@ -1,4 +1,4 @@
-package com.balietek.starterspringboot.models;
+package com.balietek.models;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
--- src/main/java/com/balietek/starterspringboot/StarterSpringBootApplication.java
+++ src/main/java/com/balietek/starterspringboot/StarterSpringBootApplication.java
@@ -3,9 +3,10 @@ package com.balietek.starterspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan(basePackages = "com.balietek.models") // sans cela les entities ne sont pas visible pour creer la base donnée
 @ComponentScan(basePackages="com.balietek") // sans cela impossible de faire des requete HTTP
 public class StarterSpringBootApplication {
