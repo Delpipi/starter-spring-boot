3ad3125caceea2109d9b41cd17f7d2e694a33b67
Delpipi <alexandrepaulkouame@gmail.com>
2023-01-03 21:21:08 +0000

    create and LocalDateTimeConverter.java

---
+++ src/main/java/com/balietek/utils/LocalDateTimeConverter.java
@@ -0,0 +1,22 @@
+package com.balietek.utils;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime,Timestamp> {
+
+	@Override
+	public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+		return attribute != null ? Timestamp.valueOf(attribute) : null;
+	}
+
+	@Override
+	public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+		return dbData != null ? dbData.toLocalDateTime() : null ;
+	}
+
+}
