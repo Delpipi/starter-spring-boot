ec40be1648ef2685593673368bc755b287e75b47
Delpipi <alexandrepaulkouame@gmail.com>
2023-01-04 22:04:22 +0000

    setup and implements jpa authentication

--- src/main/java/com/balietek/repositories/UtilisateurRepository.java
+++ src/main/java/com/balietek/repositories/UtilisateurRepository.java
@@ -3,10 +3,10 @@ package com.balietek.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import com.balietek.exceptions.ResourceNoFoundException;
 import com.balietek.models.Utilisateur;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long>{
-    Optional<Utilisateur> findByUsername(String username) throws ResourceNoFoundException;
+    Optional<Utilisateur> findByUsername(String username) throws UsernameNotFoundException;
 }
---
+++ src/main/java/com/balietek/services/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.balietek.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.balietek.models.Utilisateur;
+import com.balietek.repositories.UtilisateurRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Utilisateur utilisateur = utilisateurRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Aucun utilisateur " + username));
+        return new MyUserDetails(utilisateur);
+    }
+
+}
