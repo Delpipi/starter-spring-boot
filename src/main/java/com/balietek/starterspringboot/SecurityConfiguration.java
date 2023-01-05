3d6ad975305b769974839ff7ead2de2abb73994e
Delpipi <alexandrepaulkouame@gmail.com>
2023-01-05 17:26:15 +0000

    create JwtRequestFilter.java and setup jwt filter in SecurityConfiguration.java

--- src/main/java/com/balietek/starterspringboot/SecurityConfiguration.java
+++ src/main/java/com/balietek/starterspringboot/SecurityConfiguration.java
@@ -1,24 +1,34 @@
 package com.balietek.starterspringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.balietek.filters.JwtRequestFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeHttpRequests()
-        .requestMatchers("/**").permitAll()
-        .anyRequest().authenticated();
+        .requestMatchers("/users/register", "/users/authenticate").permitAll()
+        .anyRequest().authenticated().and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
